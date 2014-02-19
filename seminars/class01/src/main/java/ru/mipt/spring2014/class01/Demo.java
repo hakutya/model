7b1ee16f442990b72dabd8880c03677067b30f04
devianvii <ilya.plakitin@gmail.com>
2014-02-19 23:07:25 +0400

    Revert "1"
    This reverts commit 30cd759243126e6dcb83a5ed30cbf0b2fb1ac2ad.

--- seminars/class01/src/main/java/ru/mipt/spring2014/class01/Demo.java
+++ seminars/class01/src/main/java/ru/mipt/spring2014/class01/Demo.java
@@ -1,17 +1,17 @@
-
+/**
+ * Demo.java
+ * Created on Feb 13, 2014
+ */
 package ru.mipt.spring2014.class01;
 
 public class Demo
 {
-
 	public static void main (String[] args)
 	{
+		System.out.println ("ComplextNumber class demo");
 
-		ComplexNumber c1 = new ComplexNumber (5, 3), c2 = new ComplexNumber (5, 3), c3 = new ComplexNumber (3, 3);
+		ComplexNumber c1 = new ComplexNumber (5, 3), c2 = new ComplexNumber (2, -1);
 
-		System.out.println ("1 и 2" + ((c1.equals (c2)) ? " Равны" : " Не равны"));
-		System.out.println ("1 и 3" + ((c1.equals (c3)) ? " Равны" : " Не равны"));
-		System.out.println ("3 и 2" + ((c3.equals (c2)) ? " Равны" : " Не равны"));
-
+		System.out.println ("(" + c1 + ") + (" + c2 + ") = " + c1.add (c2));
 	}
 }
